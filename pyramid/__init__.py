51ec9d7416e4909041bd0816972d7ce68bd54ef6
caron <j.caron@fz-juelich.de>
2019-11-04 16:13:51 +0100

    Changes hg_revision logging to git_revision removed last artefact of hg usage ages ago... removed syntax error in setup.py...

--- pyramid/__init__.py
+++ pyramid/__init__.py
@@ -62,11 +62,11 @@ from .ramp import *
 from .quaternion import *
 from .file_io import *
 from .version import version as __version__
-from .version import hg_revision as __hg_revision__
+from .version import git_revision as __git_revision__
 
 import logging
 _log = logging.getLogger(__name__)
-_log.info("Starting Pyramid V{} HG{}".format(__version__, __hg_revision__))
+_log.info("Starting Pyramid V-{} GIT-{}".format(__version__, __git_revision__))
 del logging
 
 __all__ = ['analytic', 'magcreator', 'reconstruction', 'fieldconverter',
